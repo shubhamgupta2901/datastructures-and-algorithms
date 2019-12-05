1dfb0604b2786d31398ac9f234470f96c46e28f4
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-12-06 00:54:52 +0530

    Adding circular single linked list

---
+++ src/CircularLinkedListMain.java
@@ -0,0 +1,23 @@
+import ds.CircularLinkedList;
+
+public class CircularLinkedListMain {
+    public static void main(String[] args) {
+        CircularLinkedList list = new CircularLinkedList();
+        list.insertAtBeginning(4);
+        list.insertAtEnd(5);
+        list.insertAtBeginning(3);
+        list.insertAtBeginning(2);
+        list.insertAtBeginning(0);
+        list.insertAtEnd(6);
+        list.insertAtPosition(1,1);
+        list.insertAtPosition(7,7);
+        list.traverseList();
+
+        boolean found = list.searchNode(0) != null ? true :false;
+        System.out.println("found 0:"+found);
+        found = list.searchNode(7) != null ? true :false;
+        System.out.println("found 7:"+found);
+        found = list.searchNode(9) != null ? true :false;
+        System.out.println("found 9:"+found);
+    }
+}
