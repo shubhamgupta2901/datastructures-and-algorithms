e2a126c412fc9a9038f9b40b54328b0c6237c10f
Shubham Gupta <shubham.sg2901@gmail.com>
2019-12-19 23:02:12 +0530

    adding package for leetcode solutions

---
+++ leetcode/LinkedListCycle.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import java.util.HashSet;
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle/
+ * Given a linked list, determine if it has a cycle in it.
+ * <p>
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+ * <p>
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ */
+
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode ptr = head;
+        HashSet<ListNode> hashSet = new HashSet<ListNode>();
+        hashSet.add(ptr);
+        while (ptr.next != null) {
+            if (hashSet.contains(ptr.next))
+                return true;
+            hashSet.add(ptr.next);
+            ptr = ptr.next;
+        }
+        return false;
+    }
+}
---
+++ leetcode/TwoSum.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i]))
+                return new int[]{map.get(target-nums[i]),i};
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
