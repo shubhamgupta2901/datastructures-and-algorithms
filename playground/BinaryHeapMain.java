b47ceb94f0693b5bda3a9ce0d8c1a671176aaf6c
Shubham Gupta <shubham.sg2901@gmail.com>
2019-12-29 21:58:59 +0530

    testing binary heaps

--- playground/BinaryHeapMain.java
+++ playground/BinaryHeapMain.java
@@ -1,4 +1,29 @@
 package playground;
 
+import datastructures.BinaryHeap;
+
 public class BinaryHeapMain {
+    public static void main(String[] args) {
+        BinaryHeapMain demo = new BinaryHeapMain();
+        demo.testBinaryHeap();
+    }
+
+    private void testBinaryHeap(){
+        BinaryHeap heap = new BinaryHeap();
+        int[] values = new int[]{13,20,15,7,10,9,2,4,8,9,1};
+        for (int value: values){
+            System.out.println("Inserting "+ value+" : ");
+            heap.insert(value);
+            BinaryHeapPrinter.print(heap.getHeap());
+        }
+
+        System.out.println("--------------------");
+        System.out.println("Extract mins:");
+        while (heap.size()>0){
+            System.out.println("Get min: " + heap.extractMin());
+            BinaryHeapPrinter.print(heap.getHeap());
+        }
+
+
+    }
 }
--- playground/BinaryHeapPrinter.java
+++ playground/BinaryHeapPrinter.java
@@ -1,4 +1,30 @@
 package playground;
 
+import datastructures.interfaces.IBinaryTree;
+
 public class BinaryHeapPrinter {
+
+    public static void print(int[] heap){
+        BinaryHeapPrinter printer = new BinaryHeapPrinter();
+        IBinaryTree.TreeNode root = printer.insertLevelOrder(heap,null,0);
+        BinaryTreePrinter.print(root);
+
+    }
+
+    public IBinaryTree.TreeNode insertLevelOrder(int[] arr, IBinaryTree.TreeNode root, int i) {
+        // Base case for recursion
+        if (i < arr.length) {
+            IBinaryTree.TreeNode temp = new IBinaryTree.TreeNode(arr[i]);
+            root = temp;
+
+            // insert left child
+            root.left = insertLevelOrder(arr, root.left,
+                    2 * i + 1);
+
+            // insert right child
+            root.right = insertLevelOrder(arr, root.right,
+                    2 * i + 2);
+        }
+        return root;
+    }
 }
