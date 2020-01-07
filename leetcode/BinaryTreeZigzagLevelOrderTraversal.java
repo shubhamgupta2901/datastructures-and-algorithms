abfa80f6d86c05e0e2b65fdd6c3c475e66021dad
Shubham Gupta <shubham.sg2901@gmail.com>
2020-01-07 12:52:17 +0530

    Binary Tree Zig Zag Level Order Traversal

---
+++ leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * Using two stacks left and right to maintain the order of traversed nodes.
+     * In one level we will traverse from left to right and keep track of nodes in leftStack.
+     * In next level we will traverse from right to left and keep track of nodes in rightStack.
+     * @param root
+     * @return
+     * Time Complexity: O(n)
+     * Space Complexity: O(n) Although using two stacks but we need to store only n elements.
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> traversal = new ArrayList<>();
+        if(root == null)
+            return traversal;
+        Stack<TreeNode> leftStack = new Stack<>();
+        Stack<TreeNode> rightStack = new Stack<>();
+        rightStack.push(root);
+        boolean isLeft = true;
+        while (!leftStack.isEmpty() || !rightStack.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            TreeNode curr;
+            if(isLeft){
+                while (!rightStack.isEmpty()){
+                    curr = rightStack.pop();
+                    level.add(curr.val);
+                    if(curr.left != null)
+                        leftStack.push(curr.left);
+                    if(curr.right != null)
+                        leftStack.push(curr.right);
+                }
+            }else {
+                while (!leftStack.isEmpty()){
+                    curr = leftStack.pop();
+                    level.add(curr.val);
+                    if(curr.right != null)
+                        rightStack.push(curr.right);
+                    if(curr.left != null)
+                        rightStack.push(curr.left);
+                }
+            }
+            isLeft = !isLeft;
+            traversal.add(level);
+        }
+        return traversal;
+    }
+}
