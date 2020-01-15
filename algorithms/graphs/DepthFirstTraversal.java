a0c5663d7b5c7766c3e012e0e0b2a1e8db21a123
Shubham Gupta <shubham.sg2901@gmail.com>
2020-01-15 19:02:54 +0530

    DFS iterative and recursive

---
+++ algorithms/graphs/DepthFirstTraversal.java
@@ -0,0 +1,90 @@
+package algorithms.graphs;
+
+import datastructures.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Depth First Search (or Traversal) is an algorithm for traversing graphs.
+ * It starts at the starting node and keeps exploring as far as possible along each edge before backtracking.
+ *
+ * It is similar to Depth First Traversal of a tree. The only catch here is, unlike trees,
+ * graphs may contain cycles,so we may come to the same node again.
+ * To avoid processing a node more than once, we use a boolean visited array.
+ *
+ */
+public class DepthFirstTraversal {
+
+    /**
+     * Recursive approach to perform traversal
+     * For graph of n vertices
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(n) for recursive stack.
+     * This time complexity can be improved if we use adjacency list to represent graphs
+     * @param graph
+     * @param startingVertex
+     * @return
+     */
+    public List<Integer> performTraversal(Graph graph, int startingVertex){
+        List<Integer> traversal = new ArrayList<>();
+        if(graph.getVertexCount()<=0 || startingVertex >= graph.getVertexCount())
+            return traversal;
+        boolean[] visited = new boolean[graph.getVertexCount()];
+        dfs(graph,traversal,visited,startingVertex);
+        return traversal;
+
+    }
+
+    private void dfs(Graph graph, List<Integer> traversal, boolean[] visited, int vertex){
+        traversal.add(vertex);
+        visited[vertex] = true;
+        for(int i = 0; i<graph.getVertexCount(); i++){
+            if(graph.isEdge(vertex,i) && !visited[i])
+                dfs(graph,traversal,visited,i);
+        }
+    }
+
+    /**
+     * Iterative approach to perform traversal
+     * For graph of n vertices
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(n) for recursive stack.
+     * This time complexity can be improved if we use adjacency list to represent graphs
+     * @param graph
+     * @param startingVertex
+     * @return
+     */
+    public List<Integer> performIterativeTraversal(Graph graph, int startingVertex){
+        List<Integer> traversal = new ArrayList<>();
+        if(graph.getVertexCount()<= 0 || startingVertex >= graph.getVertexCount())
+            return traversal;
+        boolean[] visited = new boolean[graph.getVertexCount()];
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(startingVertex);
+        visited[startingVertex] = true;
+        traversal.add(startingVertex);
+
+        while(!stack.isEmpty()){
+            int vertex = stack.peek();
+            int i;
+            for(i = 0; i<graph.getVertexCount(); i++){
+                if(graph.isEdge(vertex,i) && !visited[i]){
+                    stack.push(i);
+                    visited[i] = true;
+                    traversal.add(i);
+                    break;
+                }
+            }
+
+            if(i == graph.getVertexCount())
+                stack.pop();
+
+        }
+        return traversal;
+
+    }
+}
+
--- playground/GraphMain.java
+++ playground/GraphMain.java
@@ -1,18 +1,27 @@
 package playground;
 
 import algorithms.graphs.BreadthFirstTraversal;
+
+import algorithms.graphs.DepthFirstTraversal;
 import datastructures.Graph;
+
 
 public class GraphMain {
 
     public static void main(String[] args) {
         GraphMain demo = new GraphMain();
-        demo.testGraph1();
-        demo.testGraph2();
+        BreadthFirstTraversal bfs = new BreadthFirstTraversal();
+        DepthFirstTraversal dfs = new DepthFirstTraversal();
+
+        demo.testGraph1(bfs, dfs);
+        System.out.println("-------------");
+        demo.testGraph2(bfs, dfs);
+        System.out.println("-------------");
+        demo.testGraph3(bfs, dfs);
     }
 
     // Undirected Unweighted graph
-    public void testGraph1(){
+    public void testGraph1(BreadthFirstTraversal bfs, DepthFirstTraversal dfs){
         int vertexCount = 5;
         Graph graph = new Graph(vertexCount);
 
@@ -30,12 +39,15 @@ public class GraphMain {
         graph.addEdge(2,4);
         graph.addEdge(4,2);
 
-        //graph.printGraph();
-        BreadthFirstTraversal bfs = new BreadthFirstTraversal();
+        System.out.println("BFS: ");
         Utils.printList(bfs.performTraversal(graph,0));
+        System.out.println("DFS (recursive): ");
+        Utils.printList(dfs.performTraversal(graph,0));
+        System.out.println("DFS (iterative): ");
+        Utils.printList(dfs.performIterativeTraversal(graph,0));
     }
 
-    public void testGraph2(){
+    public void testGraph2(BreadthFirstTraversal bfs, DepthFirstTraversal dfs){
         int vertexCount = 4;
         Graph graph = new Graph(vertexCount);
 
@@ -46,8 +58,55 @@ public class GraphMain {
         graph.addEdge(2,3);
         graph.addEdge(3,3);
 
-        //graph.printGraph();
-        BreadthFirstTraversal bfs = new BreadthFirstTraversal();
+        System.out.println("BFS: ");
         Utils.printList(bfs.performTraversal(graph,2));
+        System.out.println("DFS (recursive): ");
+        Utils.printList(dfs.performTraversal(graph,2));
+        System.out.println("DFS (iterative): ");
+        Utils.printList(dfs.performIterativeTraversal(graph,2));
+    }
+
+    public void testGraph3(BreadthFirstTraversal bfs, DepthFirstTraversal dfs){
+        int vertexCount = 9;
+        Graph graph = new Graph(vertexCount);
+
+        graph.addEdge(0,1);
+        graph.addEdge(0,8);
+
+        graph.addEdge(1,0);
+
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(2,5);
+        graph.addEdge(2,8);
+
+        graph.addEdge(3,2);
+
+        graph.addEdge(4,2);
+        graph.addEdge(4,7);
+
+        graph.addEdge(5,2);
+        graph.addEdge(5,6);
+
+
+        graph.addEdge(6,5);
+        graph.addEdge(6,7);
+        graph.addEdge(6,8);
+
+        graph.addEdge(7,4);
+        graph.addEdge(7,6);
+
+        graph.addEdge(8,0);
+        graph.addEdge(8,2);
+        graph.addEdge(8,6);
+
+
+        System.out.println("BFS: ");
+        Utils.printList(bfs.performTraversal(graph,0));
+        System.out.println("DFS (recursive): ");
+        Utils.printList(dfs.performTraversal(graph,0));
+        System.out.println("DFS (iterative): ");
+        Utils.printList(dfs.performIterativeTraversal(graph,0));
+
     }
 }
