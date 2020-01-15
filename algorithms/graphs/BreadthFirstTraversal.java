2fc442811b36e7b8978e237d893a9bb3f6d74f98
Shubham Gupta <shubham.sg2901@gmail.com>
2020-01-15 13:20:24 +0530

    notes on bfs

--- algorithms/graphs/BreadthFirstTraversal.java
+++ algorithms/graphs/BreadthFirstTraversal.java
@@ -4,8 +4,29 @@ import datastructures.Graph;
 
 import java.util.*;
 
+/**
+ * Breadth First Traversal (or Search) is an algorithm for traversing graph data structures.
+ * It starts at some arbitrary node of a graph and explores the neighbour nodes first,
+ * before moving to the next level of neighbours.
+ *
+ * BFS for a graph is similar to Breadth First Traversal of a binary tree.
+ * The only catch here is, unlike trees, graphs may contain cycles, so we may come to the same node again.
+ * To avoid processing a node more than once, we use a boolean visited array or a hashset.
+ *
+ *
+ */
 public class BreadthFirstTraversal {
 
+    /**
+     * Note it is assumed that all vertices are reachable from the starting vertex (Connected graphs).
+     * For disconnected graphs we will have to perform BFS for all nodes till all nodes are visited.
+     * Time complexity of BFS depends on the data structure we are using.
+     * For a graph with n vertices represented by Adjacency Matrix
+     * Time Complexity: O(n^2)
+     * @param graph
+     * @param startingVertex
+     * @return
+     */
     public List<Integer> performTraversal(Graph graph, int startingVertex){
         List<Integer> traversal = new ArrayList<>();
 
