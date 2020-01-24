5221913b8a23d403b8989fd56a5a646c3d8cb771
Shubham Gupta <shubham.sg2901@gmail.com>
2020-01-24 18:10:36 +0530

    fizzbuzz

---
+++ leetcode/FizzBuzz.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/fizz-buzz/"/>
+ *
+ * Write a program that outputs the string representation of numbers from 1 to n.
+ *
+ * But for multiples of three it should output “Fizz” instead of the number
+ * and for the multiples of five output “Buzz”.
+ * For numbers which are multiples of both three and five output “FizzBuzz”.
+ */
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for(int i = 1; i<=n; i++){
+            if(i%3 == 0 && i%5 == 0)
+                list.add("FizzBuzz");
+            else if(i%3 == 0)
+                list.add("Fizz");
+            else if(i%5 == 0)
+                list.add("Buzz");
+            else
+                list.add(Integer.toString(i));
+        }
+        return list;
+    }
+}
