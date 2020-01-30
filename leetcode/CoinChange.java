608ce6adc50a68450d9fbce78c5a9bd96429b354
Shubham Gupta <shubham.sg2901@gmail.com>
2020-01-30 18:50:34 +0530

    coin change brute force recursion

---
+++ leetcode/CoinChange.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/coin-change/"/>
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * Example 1:
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ *
+ * Example 2:
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ */
+public class CoinChange {
+
+    /**
+     * Approach 1: Brute force recursion.
+     * Trying out all the combinations and finding the minimum number of coins required
+     * Time limit exceeds for larger amounts
+     */
+    public int coinChange(int[] coins, int amount) {
+        return changeHelper(coins, amount);
+    }
+
+    private int changeHelper(int[] coins, int amount){
+        if(amount < 0 || coins.length == 0)
+            return -1;
+        if(amount == 0)
+            return 0;
+        int minChange = Integer.MAX_VALUE;
+        for(int i = 0; i<coins.length;i++){
+            int change = changeHelper(coins, amount-coins[i]);
+            if(change==-1)
+                continue;
+            change = 1 + change;
+            if(change< minChange)
+                minChange = change;
+        }
+        return minChange == Integer.MAX_VALUE ? -1: minChange;
+    }
+
+    public static void main(String[] args) {
+        int[] coins = new int[]{1,2,5};
+        int amount = 100;
+        CoinChange cc = new CoinChange();
+        int minChange = cc.coinChange(coins, amount);
+        System.out.println(minChange);
+
+    }
+}
