bc3c857ff28569f7f8060038f5c24362f1ef1609
Shubham Gupta <shubham.sg2901@gmail.com>
2020-01-31 19:20:07 +0530

    target sum adding memoization

--- leetcode/TargetSum.java
+++ leetcode/TargetSum.java
@@ -78,5 +78,50 @@ public class TargetSum {
         return ways;
     }
 
+    /**
+     * Approach 3: memoizing approach 2
+     * a 2d array where arr[index][target] refers to the number of ways of getting
+     * target if we start from index "index" in array
+     * Note the minimum value of target can be target-sum of nums array,
+     * and max value can be target+sum of nums array.
+     * so the length would be from target-sum to target+sum.
+     * Also (target-sum) can yield a negative value so
+     * we create an offset to be added before accessing indices
+     *
+     */
+    public int findTargetSumWaysApproach3(int[] nums, int S) {
+        int sum = 0;
+        for(int i =0; i<nums.length; i++)
+            sum+= nums[i];
+        Integer[][] cache = new Integer[nums.length][2*sum+1];
+        int offset = sum-S;
+        return helper(nums,offset, S, 0, cache);
+    }
+
+    private int helper(int[] nums,int offset, int target,int index, Integer[][] cache){
+        if(index >= nums.length){
+            if(target == 0)
+                return 1;
+            return 0;
+        }
+        if(cache[index][target+offset] != null)
+            return cache[index][target+offset];
+        int ways = helper(nums,offset, target+nums[index],index+1, cache) +
+                helper(nums,offset, target-nums[index], index+1, cache);
+        cache[index][target+offset] = ways;
+        return ways;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int target = 10;
+
+        int [] arr1 = new int []{1,1,1,1,1};
+        int target1 = 3;
+        int ways = new TargetSum().findTargetSumWaysApproach2(arr1, target1);
+        System.out.println(target);
+    }
+
+
 
 }
