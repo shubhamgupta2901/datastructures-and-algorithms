8a99dddc5a1c8acdf0cdda9e50c7d0d61eb3a4b7
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-02 17:17:29 +0530

    minimum subset sum difference another brute force

--- algorithms/dp/knapsack/MinimumSubsetSumDifference.java
+++ algorithms/dp/knapsack/MinimumSubsetSumDifference.java
@@ -31,7 +31,7 @@ public class MinimumSubsetSumDifference {
      * Time complexity: O(2^n)
      * Space Complexity: O(n) for recursive stack, because at any point the stack will have maximum n elements.
      */
-    public int findDifference(int []set){
+    public int findDifferenceApproach1(int []set){
         int sum = 0;
         for(int i = 0; i<set.length; i++)
             sum+=set[i];
@@ -47,14 +47,39 @@ public class MinimumSubsetSumDifference {
     }
 
 
+    /**
+     * Approach 2: Same as approach 1 but a modification in thinking process.
+     * I don't need to maintain the remainder of sum of array
+     * Because remainder will always be total sum of array - total sum of subset.
+     * And the difference between the sum of current subset and the sum of rest of elements will be
+     * difference of sum between subsets = |sum of current subset - sum of remainder of array|
+     * Or |sum of current subset - (total sum of array - sum of current subset)|
+     * Time complexity: O(2^n)
+     * Space Complexity: O(n)
+     */
+    public int findDifferenceApproach2(int[] set) {
+        int sum = 0;
+        for(int i = 0; i<set.length; i++)
+            sum+=set[i];
+        return this.findDifferenceHelper(set,sum, 0,0);
+    }
+
+    private int findDifferenceHelper2(int [] set,int totalSum, int index, int subsetSum){
+        if(index == set.length)
+            return Math.abs(totalSum-2*subsetSum);
+        return Math.min(findDifferenceHelper2(set,totalSum,index+1,subsetSum),
+                findDifferenceHelper2(set, totalSum,index+1,subsetSum+set[index]));
+    }
+
+
     public static void main(String[] args) {
         MinimumSubsetSumDifference ps = new MinimumSubsetSumDifference();
         int[] num = {1, 2, 3, 9};
-        System.out.println(ps.findDifference(num));
+        System.out.println(ps.findDifferenceApproach1(num));
         num = new int[]{1, 2, 7, 1, 5};
-        System.out.println(ps.findDifference(num));
+        System.out.println(ps.findDifferenceApproach1(num));
         num = new int[]{1, 3, 100, 4};
-        System.out.println(ps.findDifference(num));
+        System.out.println(ps.findDifferenceApproach1(num));
     }
 
 }
