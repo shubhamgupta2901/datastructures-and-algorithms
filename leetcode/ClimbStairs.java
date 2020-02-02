dd2cd3c4dd400871ed130566aba4bab8f9ce81f1
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-03 01:30:28 +0530

    climb stairs memoization

--- leetcode/ClimbStairs.java
+++ leetcode/ClimbStairs.java
@@ -31,12 +31,40 @@ public class ClimbStairs {
      * Space Complexity: O(n) because at any stage
      * there can be at max n numbers in recursive stack
      */
-    public int climbStairs(int n) {
+    public int climbStairsApproach1(int n) {
         if( n == 0)
             return 1;
-        int ways = climbStairs(n-1);
+        int ways = climbStairsApproach1(n-1);
         if(n-2>=0)
-            ways+=climbStairs(n-2);
+            ways+=climbStairsApproach1(n-2);
         return ways;
     }
+
+    /**
+     * Approach 2: Top down memoization - Accepted
+     * Time Complexity: ? Certainly not O(2^n)
+     * because a lot of overlapping subproblems are cached in array.
+     * Runtime percentile is 100%
+     * But need to check what exactly is asymptotic time complexity.
+     * Space Complexity: O(n) cache array and recursive stack.
+     * Memory usage is ~5 percentile.
+     */
+    public int climbStairsApproach2(int n) {
+        Integer[] cache = new Integer[n+1];
+        return helper(n, cache);
+    }
+
+    private int helper(int n, Integer[] cache){
+        if(n == 0)
+            return 1;
+        if(cache[n]!=null)
+            return cache[n];
+        int ways = helper(n-1, cache);
+        if(n-2>=0)
+            ways+= helper(n-2, cache);
+        cache[n] = ways;
+        return ways;
+    }
+
+
 }
