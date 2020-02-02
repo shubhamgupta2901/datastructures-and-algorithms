c9971f32a7ba8f35398e226d8b449e24b93deedc
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-02 21:23:45 +0530

    minimum path sum dp memoization

--- leetcode/MinimumPathSum.java
+++ leetcode/MinimumPathSum.java
@@ -19,7 +19,7 @@ package leetcode;
  */
 public class MinimumPathSum {
     /**
-     * Approach 1: Brute force approach
+     * Approach 1: Brute force approach - Time limit exceeds
      * Try all the possible moves and find minimum path sum.
      * Solution seems correct but time limit exceeds for larger grids.
      * Time complexity : ? Its evident that its exponential O(2^N) because at taking two decisions,
@@ -38,6 +38,31 @@ public class MinimumPathSum {
         if(i == grid.length-1 && j == grid[0].length-1)
             return grid[grid.length-1][grid[0].length-1];
         int sum = grid[i][j] + Math.min(helper(grid,i+1,j),helper(grid, i, j+1));
+        return sum;
+    }
+
+    /**
+     * Approach 2: Dynamic Programming memoization - Accepted
+     * Its difficult for me to say, how this code works and has 99.4% time complexity in leetcode.
+     * Memoization is generally used when you can see overlapping subproblems
+     * and you cache them to save time. Here I could not see any overlapping subproblems.
+     * Time Complexity: O(mn)
+     * Space Complexity: O(mn)
+     */
+    public int minPathSumApproach2(int[][] grid) {
+        Integer[][]cache = new Integer[grid.length][grid[0].length];
+        return helper(grid, 0, 0, cache);
+    }
+
+    private int helper(int[][]grid, int i, int j, Integer[][] cache){
+        if(i>= grid.length || j>= grid[0].length)
+            return Integer.MAX_VALUE;
+        if(i == grid.length-1 && j == grid[0].length-1)
+            return grid[grid.length-1][grid[0].length-1];
+        if(cache[i][j]!=null)
+            return cache[i][j];
+        int sum = grid[i][j] + Math.min(helper(grid,i+1,j, cache),helper(grid, i, j+1,cache));
+        cache[i][j] = sum;
         return sum;
     }
 }
