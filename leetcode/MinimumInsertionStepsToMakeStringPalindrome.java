5c79b68fa0b5736bb7e3a6f613a92599a7b2417d
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-06 16:05:17 +0530

    min insertion to make string palindrome brute force

---
+++ leetcode/MinimumInsertionStepsToMakeStringPalindrome.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+/**
+ * <a href = "https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/"/>
+ * Given a string s. In one step you can insert any character at any index of the string.
+ * Return the minimum number of steps to make s palindrome.
+ * A Palindrome String is one that reads the same backward as well as forward.
+ *
+ * Example 1:
+ * Input: s = "zzazz"
+ * Output: 0
+ * Explanation: The string "zzazz" is already palindrome we don't need any insertions.
+ *
+ * Example 2:
+ * Input: s = "mbadm"
+ * Output: 2
+ * Explanation: String can be "mbdadbm" or "mdbabdm".
+ *
+ * Example 3:
+ * Input: s = "leetcode"
+ * Output: 5
+ * Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+ *
+ * Example 4:
+ * Input: s = "g"
+ * Output: 0
+ *
+ * Example 5:
+ * Input: s = "no"
+ * Output: 1
+ *
+ * Constraints:
+ * 1 <= s.length <= 500
+ * All characters of s are lower case English letters.
+ */
+public class MinimumInsertionStepsToMakeStringPalindrome {
+
+    /**
+     * Approach 1: Brute force - time limit exceeds
+     * Note that this problem is exactly same to minimum number of deletions in string to make it palindrome.
+     * Approach: Take string, characters at its startIndex and endIndex.
+     * If characters at start and end indices are same then check how many characters need to be deleted in its substring
+     * start+1 to end-1.
+     * If they are not similar, then one character needs to added which gives us two options:
+     * 1) suppose we add character at end index to before start index, then we check how many characters need to deleted
+     * in substring(start, end-1).
+     * 2) suppose we add character at start index after end index , then we check how many characters need to deleted
+     *  in substring(start+1, end).
+     * 3) we return 1 + min(step1, step2)
+     * Time Complexity: O(2^n)
+     * Space Complexity: O(2^n)
+     */
+    public int minInsertionsApproach1(String s) {
+        return helper(s, 0, s.length()-1);
+    }
+
+    private int helper(String s, int startIndex, int endIndex){
+        if(startIndex>= endIndex)
+            return 0;
+        if(s.charAt(startIndex) == s.charAt(endIndex))
+            return helper(s, startIndex+1, endIndex-1);
+        int minInsertions = 1 + Math.min(helper(s, startIndex+1, endIndex),
+                helper(s,startIndex, endIndex-1));
+        return minInsertions;
+    }
+}
