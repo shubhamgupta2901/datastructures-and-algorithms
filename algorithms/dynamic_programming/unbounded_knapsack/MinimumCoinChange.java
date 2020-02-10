cb0505b186cacbd2646321ddc8e4572d45b8ae2b
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-10 23:56:57 +0530

    minimum coin change memoization

--- algorithms/dynamic_programming/unbounded_knapsack/MinimumCoinChange.java
+++ algorithms/dynamic_programming/unbounded_knapsack/MinimumCoinChange.java
@@ -48,4 +48,59 @@ public class MinimumCoinChange {
         return Math.min(change1, change2);
 
     }
+
+
+    /**
+     * Approach 1: Brute force Approach
+     * Time Complexity: O(n*T) where n is the length of denominations, T is target
+     * Space Complexity: O(n*T)
+     */
+    private int findMinCoinChangeApproach2(int[] denominations, int total){
+        Integer[][] cache = new Integer[denominations.length][total+1];
+        return helper(denominations, total, 0,cache);
+    }
+
+    private int helper(int[] denominations,int total, int index, Integer[][]cache){
+        //base case:
+        if(total == 0)
+            return 0;
+        if(index == denominations.length)
+            return Integer.MAX_VALUE;
+        //memoization
+        if(cache[index][total]!=null)
+            return cache[index][total];
+        //recursion
+        int change1 =  Integer.MAX_VALUE;
+        if(total>= denominations[index]){
+            int change = helper(denominations, total-denominations[index],index, cache);
+            if(change!= Integer.MAX_VALUE)
+                change1 =  1 + change;
+        }
+
+        int change2 = helper(denominations, total, index+1, cache);
+
+        //return
+        cache[index][total] = Math.min(change1, change2);
+        return cache[index][total];
+
+    }
+
+    public static void main(String[] args) {
+        MinimumCoinChange cc = new MinimumCoinChange();
+        int[] denominations = {1, 2, 3};
+        System.out.println("Brute Force");
+        System.out.println(cc.findMinCoinChangeApproach1(denominations, 5));
+        System.out.println(cc.findMinCoinChangeApproach1(denominations, 11));
+        System.out.println(cc.findMinCoinChangeApproach1(denominations, 7));
+        denominations = new int[]{3, 5};
+        System.out.println(cc.findMinCoinChangeApproach1(denominations, 7));
+        System.out.println("-------------------");
+        System.out.println("Memoization");
+        denominations = new int[]{1, 2, 3};
+        System.out.println(cc.findMinCoinChangeApproach2(denominations, 5));
+        System.out.println(cc.findMinCoinChangeApproach2(denominations, 11));
+        System.out.println(cc.findMinCoinChangeApproach2(denominations, 7));
+        denominations = new int[]{3, 5};
+        System.out.println(cc.findMinCoinChangeApproach2(denominations, 7));
+    }
 }
