a12f5feec4530c3414a83d65b71aa022f38b2c02
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-11 17:15:19 +0530

    min jumps to reach end top down dp

--- algorithms/dynamic_programming/fibonacci/MinimumJumpsToReachEnd.java
+++ algorithms/dynamic_programming/fibonacci/MinimumJumpsToReachEnd.java
@@ -25,8 +25,11 @@ public class MinimumJumpsToReachEnd {
      * So, if the value at the current index is ‘p’, we will try every jump in the range (1 to ‘p’) from that index.
      * After taking a jump, we recursively try all option from that index.
      *
+     * Time complexity: O(2^n) where ‘n’ is the size of the input array.
+     * Need to check how for loop effects the time complexity.
+     * The space complexity is O(n) which is used to store the recursion stack.
      */
-    private int countMinJumps(int[] jumps){
+    private int countMinJumpsApproach1(int[] jumps){
         int minJumps =  helper(jumps, 0);
         return minJumps == Integer.MAX_VALUE ? -1 : minJumps;
     }
@@ -45,12 +48,40 @@ public class MinimumJumpsToReachEnd {
         return minJumps;
     }
 
+    /**
+     * Approach 2: Top Down Dynamic Programming - Memoization
+     * saving the results of subproblems in an array, so that overlapping subproblems don't need
+     * to be solved again.
+     */
+    private int countMinJumpsApproach2(int[] jumps){
+        Integer[] cache = new Integer[jumps.length];
+        int minJumps =  helper(jumps, 0, cache);
+        return minJumps == Integer.MAX_VALUE ? -1 : minJumps;
+    }
+
+    private int helper(int[] jumps, int index, Integer[] cache){
+        if(index >= jumps.length-1)
+            return 0;
+        if(jumps[index] == 0)
+            return Integer.MAX_VALUE;
+        if(cache[index]!=null)
+            return cache[index];
+        int minJumps = Integer.MAX_VALUE;
+        for(int i = 1; i<=jumps[index]; i++){
+            int jump = helper(jumps, index+i, cache);
+            if(jump !=Integer.MAX_VALUE && (1+jump) < minJumps)
+                minJumps = 1 + jump;
+        }
+        cache[index] = minJumps;
+        return minJumps;
+    }
+
     public static void main(String[] args) {
         MinimumJumpsToReachEnd aj = new MinimumJumpsToReachEnd();
         int[] jumps = {2, 1, 1, 1, 4};
-        System.out.println(aj.countMinJumps(jumps));
+        System.out.println(aj.countMinJumpsApproach2(jumps));
         jumps = new int[]{1, 1, 3, 6, 9, 3, 0, 1, 3};
-        System.out.println(aj.countMinJumps(jumps));
+        System.out.println(aj.countMinJumpsApproach2(jumps));
     }
 
 
