0a64761467f442325298ce32565df631468e6f2a
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-15 16:32:12 +0530

    Unique Paths II bottom up dp

--- leetcode/UniquePathsII.java
+++ leetcode/UniquePathsII.java
@@ -51,4 +51,35 @@ public class UniquePathsII {
         cache[m][n] =  ways1 + ways2;
         return cache[m][n];
     }
+
+    /**
+     * Approach 2: Bottom up Dynamic Programming
+     * Time Complexity: O(m*n)
+     * Space Complexity: O(1) because for dp table, we are reusing obstacleGrid
+     */
+    public int uniquePathsWithObstaclesApproach2(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if(m == 0 || n == 0)
+            return 0;
+        if(obstacleGrid[0][0] == 1)
+            return 0;
+
+        for(int r = m-1; r>= 0; r--){
+            for(int c = n-1; c>=0; c--){
+                if(r == m-1 && c == n-1)
+                    obstacleGrid[r][c] = obstacleGrid[r][c] == 0 ? 1 : 0;
+                    //obstacle
+                else if(obstacleGrid[r][c] == 1)
+                    obstacleGrid[r][c] = 0;
+                else{
+                    int ways1 = r+1 < m ? obstacleGrid[r+1][c] : 0;
+                    int ways2 = c+1 < n ? obstacleGrid[r][c+1] : 0;
+                    obstacleGrid[r][c] = ways1 + ways2;
+                }
+            }
+        }
+
+        return obstacleGrid[0][0];
+    }
 }
