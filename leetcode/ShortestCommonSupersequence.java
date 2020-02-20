68c182e099a71b6f845cf1003a7ef295f5d6d046
Shubham Gupta <shubham.sg2901@gmail.com>
2020-02-20 16:21:25 +0530

    shortest common supersequence bottom up dp

--- leetcode/ShortestCommonSupersequence.java
+++ leetcode/ShortestCommonSupersequence.java
@@ -32,7 +32,7 @@ public class ShortestCommonSupersequence {
      * Also at every step two new strings are being created with possible lengths of (m+m). there are 2^(m+m) calls
      * So Space Complexity would be 0((m+m)2^(m+m))
      */
-    public String shortestCommonSupersequence(String str1, String str2) {
+    public String shortestCommonSupersequenceApproach1(String str1, String str2) {
         return helper(str1, str2, 0,0, new String(""));
     }
 
@@ -64,9 +64,50 @@ public class ShortestCommonSupersequence {
         return seq1.length() >= seq2.length() ? seq2 : seq1;
     }
 
+    /**
+     * Approach 2: Bottom up Dynamic Programming - Tabulation
+     * Memory Limit Exceeds 46/47 cases passed.
+     * Time Complexity: O(m*n)
+     * Space Complexity: O(mn(m+n)) , m*n for table and each element of table is a string
+     * of possibly m+n length.
+     */
+    public String shortestCommonSupersequenceApproach2(String str1, String str2) {
+        int M = str1.length();
+        int N = str2.length();
+        String[][] table = new String[M+1][N+1];
+
+        //table[i][j] represents the SCS in str1.substring(i) and str2.substring(j).
+        //interested in table[0][0];
+
+        //bottom case:
+        for(int i = M; i>=0; i--){
+            table[i][N] = new String(str1.substring(i));
+        }
+
+        //bottom case:
+        for(int j = N; j>=0; j--){
+            table[M][j] = new String(str2.substring(j));
+        }
+
+        for(int i = M-1; i>=0; i--){
+            for(int j = N-1; j>=0; j--){
+                if(str1.charAt(i) == str2.charAt(j))
+                    table[i][j] = new String(Character.toString(str1.charAt(i))) + table[i+1][j+1];
+                else{
+                    if(table[i+1][j].length()<= table[i][j+1].length())
+                        table[i][j] = new String(Character.toString(str1.charAt(i))) + table[i+1][j];
+                    else table[i][j] = new String(Character.toString(str2.charAt(j))) + table[i][j+1];
+                }
+            }
+        }
+
+        return table[0][0];
+
+    }
+
     public static void main(String[] args) {
         ShortestCommonSupersequence scs = new ShortestCommonSupersequence();
-        String sequence = scs.shortestCommonSupersequence("abac", "cab");
+        String sequence = scs.shortestCommonSupersequenceApproach2("abac", "cab");
         System.out.println(sequence);
     }
 }
