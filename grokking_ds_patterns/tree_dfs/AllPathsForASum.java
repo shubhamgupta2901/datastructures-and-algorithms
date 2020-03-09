2e9bba216ce756ac3bccfbdf57c7e98203f0d31c
Shubham Gupta <shubham.sg2901@gmail.com>
2020-03-09 18:49:18 +0530

    all paths for a sum in tree optimal approach

--- grokking_ds_patterns/tree_dfs/AllPathsForASum.java
+++ grokking_ds_patterns/tree_dfs/AllPathsForASum.java
@@ -27,16 +27,16 @@ public class AllPathsForASum {
     /**
      * Approach 1: Create a new List for every path - Accepted
      */
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+    public List<List<Integer>> pathSum1(TreeNode root, int sum) {
         List<List<Integer>> paths = new ArrayList<>();
         if(root == null)
             return paths;
         List<Integer> path = new ArrayList<>();
-        helper(root, paths, path, sum);
+        helper1(root, paths, path, sum);
         return paths;
     }
 
-    private void helper(TreeNode root, List<List<Integer>> paths, List<Integer> path , int sum){
+    private void helper1(TreeNode root, List<List<Integer>> paths, List<Integer> path , int sum){
         //base cases
         if(root == null)
             return;
@@ -48,9 +48,50 @@ public class AllPathsForASum {
         }
         //recursion
         path.add(root.val);
-        helper(root.left, paths, new ArrayList<Integer>(path), sum-root.val);
-        helper(root.right, paths, new ArrayList<Integer>(path), sum - root.val);
+        helper1(root.left, paths, new ArrayList<Integer>(path), sum-root.val);
+        helper1(root.right, paths, new ArrayList<Integer>(path), sum - root.val);
 
+        //return statement
+        return;
+    }
+
+    /**
+     * Approach 2: Optimizing creating new arraylists - Accepted
+     *
+     */
+    public List<List<Integer>> pathSum2(TreeNode root, int sum) {
+        List<List<Integer>> paths = new ArrayList<>();
+        if(root == null)
+            return paths;
+        List<Integer> path = new ArrayList<>();
+        helper2(root, paths, path, sum);
+        return paths;
+    }
+
+    private void helper2(TreeNode root, List<List<Integer>> paths, List<Integer> path , int sum){
+        //base cases
+        if(root == null)
+            return;
+        if(root.left == null && root.right == null && root.val == sum){
+            //TODO: create a new list, copy path in it, add to paths.
+            List<Integer> newPath = new ArrayList<>(path);
+            newPath.add(root.val);
+            paths.add(newPath);
+            return;
+        }
+
+        //recursion
+        if(root.left != null){
+            path.add(root.val);
+            helper2(root.left, paths, path, sum-root.val);
+            path.remove(path.size()-1);
+        }
+
+        if(root.right!=null){
+            path.add(root.val);
+            helper2(root.right, paths, path, sum-root.val);
+            path.remove(path.size()-1);
+        }
         //return statement
         return;
     }
