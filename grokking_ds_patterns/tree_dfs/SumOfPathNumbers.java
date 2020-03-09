0596700fd3e27a57ff044bfa59b4e39f8f0ce9cc
Shubham Gupta <shubham.sg2901@gmail.com>
2020-03-10 00:42:58 +0530

    sum of path numbers logarithmic space approach

--- grokking_ds_patterns/tree_dfs/SumOfPathNumbers.java
+++ grokking_ds_patterns/tree_dfs/SumOfPathNumbers.java
@@ -18,7 +18,8 @@ public class SumOfPathNumbers {
     /**
      * Approach 1 : Aggregate all the numbers represented by root - leaf paths, and add them
      * Time Complexity: O(n)
-     * Space Complexity:O(n). Array which contains a number corresponding to every root-leaf path.
+     * Space Complexity: O(n) For recursive stack : O(h) where h is height
+     * Additionally, Array which contains a number corresponding to every root-leaf path.
      * For a binary tree with height h, Max number of leaf nodes could be (2^h-1). So Space Complexity would
      * be O(2^h) ~ O(n)
      */
@@ -50,5 +51,34 @@ public class SumOfPathNumbers {
             helper(root.right, numbers,val);
         //return statment
         return;
+    }
+
+    /**
+     * Approach 2: Without aggregating the numbers, sum can be obtained, by returning a number when we encounter a leaf
+     * node, and add all of them.
+     * Time Complexity: O(n)
+     * Space Complexity: O(h) for recursive stack.
+     */
+    public static int findSumOfPathNumbers2(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    private static int helper(TreeNode root, int val){
+        if(root == null)
+            return 0;
+        val = val*10 + root.val;
+        if(root.left == null && root.right == null)
+            return val;
+        return helper(root.left, val) + helper(root.right, val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(1);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(5);
+        System.out.println("Total Sum of Path Numbers: " + SumOfPathNumbers.findSumOfPathNumbers1(root));
     }
 }
