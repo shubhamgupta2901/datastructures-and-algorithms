1d08fc306278a76d93651192214f651ec05013b3
Shubham Gupta <shubham.sg2901@gmail.com>
2020-03-11 20:02:49 +0530

    min depth from root-leaf bfs

--- leetcode/MinimumDepthOfBinaryTree.java
+++ leetcode/MinimumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given a binary tree, find its minimum depth.
  * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
@@ -51,5 +54,35 @@ public class MinimumDepthOfBinaryTree {
         if(root.right!=null)
             rightDepth = minDepth(root.right);
         return 1 + Math.min(leftDepth, rightDepth);
+    }
+
+    /**
+     * Approach 2: BFS approach
+     * Instead of keeping track of all the nodes in a level, we will only track the level of the tree.
+     * As soon as we find our first leaf node, that level will represent the minimum depth of the tree.
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+    public int minDepthBFS(TreeNode root) {
+        if(root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 1;
+        TreeNode node;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i<size; i++){
+                node = queue.remove();
+                if(node.left == null && node.right == null)
+                    return depth;
+                if(node.left!=null)
+                    queue.add(node.left);
+                if(node.right!=null)
+                    queue.add(node.right);
+            }
+            depth++;
+        }
+        return depth;
     }
 }
