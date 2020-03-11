05e3c4f1be2225910e6ec59ce762e90274c9bef4
Shubham Gupta <shubham.sg2901@gmail.com>
2020-03-11 20:00:01 +0530

    min depth from root-leaf dfs

---
+++ leetcode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its minimum depth = 2.
+ *
+ *    3
+ *   /
+ *  2
+ * returns its minimum depth 2
+ *
+ *   3
+ *   \
+ *   2
+ * returns its minimum depth 2
+ *
+ */
+public class MinimumDepthOfBinaryTree {
+
+    /**
+     * Approach 1: DFS
+     * Writing a recursive function :
+     * If a node is leaf return 1
+     * If, a node has both left and right children, min depth will be 1 + min(mindepth(node.left), mindepth(node.right))
+     * If node has only left child min depth will be 1 + mindepth(node.left)
+     * else if it has only right child min depth will be 1 + mindepth(node.right)
+     *
+     * Time Complexity: O(n)
+     * Space Complexity: O(h) for recursive stack where h is the height of binary tree
+     */
+    public int minDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return 1;
+        int leftDepth = Integer.MAX_VALUE;
+        if(root.left!=null)
+            leftDepth = minDepth(root.left);
+        int rightDepth = Integer.MAX_VALUE;
+        if(root.right!=null)
+            rightDepth = minDepth(root.right);
+        return 1 + Math.min(leftDepth, rightDepth);
+    }
+}
