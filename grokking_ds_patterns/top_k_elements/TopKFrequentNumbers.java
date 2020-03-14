69c73439b04e2524919bb0022d74522a1d9f71ef
Shubham Gupta <shubham.sg2901@gmail.com>
2020-03-14 18:17:45 +0530

    top k frequent elements

--- grokking_ds_patterns/top_k_elements/TopKFrequentNumbers.java
+++ grokking_ds_patterns/top_k_elements/TopKFrequentNumbers.java
@@ -1,4 +1,4 @@
-package leetcode;
+package grokking_ds_patterns.top_k_elements;
 
 import java.util.*;
 
@@ -20,70 +20,22 @@ import java.util.*;
  * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
  *
  */
-public class TopKFrequentElements {
-
-    class Pair implements Comparable{
-        int num;
-        int frequency;
-
-        Pair(int num, int frequency){
-            this.num = num;
-            this.frequency = frequency;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            return this.frequency - ((Pair)o).frequency;
-        }
-    }
+public class TopKFrequentNumbers {
 
     /**
      * Approach: We first need to know the frequency of each number, for which we can use a HashMap.
      * Once we have the frequency map, we can use a Min Heap to find the ‘K’ most frequently occurring number.
      * In the Min Heap, instead of comparing numbers we will compare their frequencies
      * in order to get frequently occurring numbers.
+     * 
+     * Also, We don't need to store both the number and its frequency as a Pair in the heap.
+     * Rather, whenever we only store the number in Heap, and when we need to know a number's frequency,
+     * we do a look-up in hashmap which takes constant time.
      *
      * Time Complexity: O(Nlogk), where N is the length of numbers.
      * We count the frequency of each number in O(N) time, then we add N numbers to the heap, each in O(logk) time.
      * Finally, we pop from the heap up to k times. As k≤N, this is O(Nlogk) in total.
      * Space Complexity: O(N) for hashMap, O(k) for heap and O(N) for output list.
-     */
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-        PriorityQueue<Pair> minHeap = new PriorityQueue<>();
-
-        for(int i = 0; i<nums.length; i++){
-            if(!frequencyMap.containsKey(nums[i]))
-                frequencyMap.put(nums[i], 1);
-            else frequencyMap.put(nums[i], frequencyMap.get(nums[i])+1);
-        }
-
-
-        for(Map.Entry<Integer, Integer> entry: frequencyMap.entrySet()){
-            Pair pair = new Pair(entry.getKey(),entry.getValue());
-            if(minHeap.size()<k)
-                minHeap.add(pair);
-            else if(minHeap.peek().frequency < pair.frequency){
-                minHeap.poll();
-                minHeap.add(pair);
-            }
-        }
-
-        List<Integer> topKFrequentElements = new ArrayList<>();
-        while(!minHeap.isEmpty())
-            topKFrequentElements.add(minHeap.poll().num);
-        return topKFrequentElements;
-
-    }
-
-    /**
-     * Approach 2: Simplifying the first solution,
-     * We don't need to store both the number and its frequency as a Pair in the heap.
-     * Rather, whenever we only store the number in Heap, and when we need to know a number's frequency,
-     * we do a look-up in hashmap which takes constant time.
-     *
-     * Time and Space complexties remain same to above approach.
-     *
      */
     public List<Integer> topKFrequent2(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
